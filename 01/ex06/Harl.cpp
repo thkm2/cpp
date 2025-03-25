6647bce0f164f2c959dfce6a718e6e5ecf4f3dd8
Kim <thkm.grd@gmail.com>
2025-03-25 14:22:19 +0100

    .

---
+++ 01/ex06/Harl.cpp
@@ -0,0 +1,38 @@
+#include "Harl.hpp"
+
+void Harl::debug( void ) {
+	std::cout << GREEN << "[ DEBUG ]" << RESET << std::endl
+	<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger."
+	<< std::endl << "I really do!"
+	<< std::endl << std::endl;
+}
+
+void Harl::info( void ) {
+	std::cout << BLUE << "[ INFO ]" << RESET << std::endl
+	<< "I cannot believe adding extra bacon costs more money."
+	<< std::endl << "You didn’t put enough bacon in my burger!"
+	<< std::endl << "If you did, I wouldn’t be asking for more!"
+	<< std::endl << std::endl;
+}
+
+void Harl::warning( void ) {
+	std::cout << YELLOW << "[ WARNING ]" << RESET << std::endl
+	<< "I think I deserve to have some extra bacon for free."
+	<< std::endl << "I’ve been coming for years, whereas you started working here just last month."
+	<< std::endl << std::endl;
+}
+
+void Harl::error( void ) {
+	std::cout << RED << "[ ERROR ]" << RESET << std::endl
+	<< "This is unacceptable! I want to speak to the manager now."
+	<< std::endl << std::endl;
+}
+
+void Harl::complain( std::string level ) {
+	void		(Harl::*ptrs[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level)
+			(this->*ptrs[i])();
+	}
+}
---
+++ 01/ex06/main.cpp
@@ -0,0 +1,37 @@
+#include "Harl.hpp"
+
+int get_index(std::string level) {
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level)
+			return (i);
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	return (-1);
+}
+
+int	main(int ac, char **av) {
+	Harl h;
+	
+
+	if (ac != 2) {
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		return (0);
+	}
+
+	int i = get_index(av[1]);
+	if (i == -1)
+		return (0);
+
+	switch (i) {
+		case 0:
+			h.complain("DEBUG");
+		case 1:
+			h.complain("INFO");
+		case 2:
+			h.complain("WARNING");
+		case 3:
+			h.complain("ERROR");
+	}
+	return (0);
+}
