930248faad1144324475a615ea76a155ba481d2c
Kim <thkm.grd@gmail.com>
2025-09-19 10:30:06 +0200

    .

---
+++ 08/ex00/easyfind.hpp
@@ -0,0 +1,18 @@
+#ifndef EASYFIND_H
+#define EASYFIND_H
+
+#include <algorithm> // find
+
+template <typename T>
+typename T::iterator easyfind(T &container, int needle) {
+	typename T::iterator it = std::find(container.begin(), container.end(), needle);
+	return it; // .end() si pas trouvé
+}
+
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int needle) {
+	typename T::const_iterator it = std::find(container.begin(), container.end(), needle);
+	return it; // pareil
+}
+
+#endif
---
+++ 08/ex00/main.cpp
@@ -0,0 +1,33 @@
+#include "easyfind.hpp"
+#include <vector>
+#include <iostream>
+#include <list>
+
+int main() {
+	std::vector<int> v;
+	v.push_back(10);
+	v.push_back(42);
+	v.push_back(80);
+	std::vector<int>::iterator it = easyfind(v, 42);
+
+	if (it != v.end()) {
+        std::cout << "Found: " << *it << "\n";
+    } else {
+        std::cout << "Not found\n";
+    }
+
+	std::list<int> l;
+	l.push_back(2);
+	l.push_back(4);
+	l.push_back(6);
+	l.push_back(8);
+	std::list<int>::iterator lit = easyfind(l, 63);
+
+	if (lit != l.end()) {
+        std::cout << "Found: " << *lit << "\n";
+    } else {
+        std::cout << "Not found\n";
+    }
+
+	return 0;
+}
